eaa7cce8be60f6b71c01a113f6cf7db019f45ee6
whosmyname <d>
2020-08-08 11:30:36 +0200

    added progress bar, more comments and restructured the code

--- ProxLoadMe.py
+++ ProxLoadMe.py
@@ -1,4 +1,4 @@
-"""Script to Download Anime Episodes from Proxer.me"""
+""" Script to Download Anime Episodes from Proxer.me """
 
 import os
 import sys
@@ -12,8 +12,7 @@ from re import search
 
 import requests
 from bs4 import BeautifulSoup, SoupStrainer
-
-# SEARCH FOR # TO FIND ALL COMMENTS
+import tqdm
 
 AUTHFILE = "login.auth"
 
@@ -43,39 +42,42 @@ LIMIT = 5
 SESSION = requests.Session()
 EXECUTOR = cf.ThreadPoolExecutor(LIMIT)
 
-def ask_overwrite(filename):
-    """ asks if a file sould be overwritten """
-    retval = input(f"The file {filename} already exists...\nSould it be overwritten?\n[y/n]")
-    if retval == "n":
-        return False
-    return True
+class NoURLError(Exception):
+    pass
 
 def download_file(srcfile, srcurl):
-    """Function to Downloadad and verify downloaded Files"""
-    if os.path.exists(srcfile):
-        if not ask_overwrite(srcfile):
-            return False
-    LOGGER.info(f"Downloading {srcurl} as {srcfile}")
-    with open(srcfile, "wb") as fifo:#open in binary write mode
-        response = SESSION.get(srcurl)#get request
-        fifo.write(response.content)#write to file
-    return True
+    """ Function to Downloadad and verify downloaded Files """
+    response = SESSION.get(srcurl, stream=True)# get request, stream data
+    content_length = int(response.headers['content-length'] or 0) # unfortunately this returns a string, easy conversion or 0
+    if os.path.exists(srcfile) and os.path.getsize(srcfile) < content_length: # check if there was an previously correctly downloaded file
+        os.remove(srcfile)
+    LOGGER.debug(f"Downloading {srcurl} as {srcfile}")
+    with open(srcfile, "wb") as fifo:# open in binary write mode
+        if content_length == 0: # check if unusable, no conversion needed
+            fifo.write(response.content)#write to file
+        else: # write with progressbar
+            progbar = tqdm.tqdm(total=content_length, unit_scale=True, desc=srcfile.split(SLASH)[len(srcfile.split(SLASH)) - 1], unit="bytes")
+            progbar.get_lock()
+            for chunk in response.iter_content(4096): # iterate the response, writing to file and updating the progressbar
+                fifo.write(chunk)
+                progbar.update(len(chunk))
+            progbar.close()
 
 def init_preps():
-    """Function to log in and initiate the Download Process"""
+    """ Function to log in and initiate the Download Process """
 
     config = ConfigParser()
-    try:
+    try: # safely try to read login credentials and log into proxer
         config.read(AUTHFILE)
         user = config["LOGIN"]["USER"]
         passwd = config["LOGIN"]["PASS"]
         #LOGGER.info(f"{user}|{passwd}")
-        resp = SESSION.get("https://proxer.me")
-        strainer = SoupStrainer(id="loginBubble")
-        soup = BeautifulSoup(resp.content, "html.parser", parse_only=strainer)
-        url = soup.find("form")["action"]
-        creds = {"username": user, "password":passwd, "remember":1}
-        resp2 = SESSION.post(url, data=creds)
+        resp = SESSION.get("https://proxer.me") # grab the main page
+        strainer = SoupStrainer(id="loginBubble") # restrict to login related html using a strainer
+        soup = BeautifulSoup(resp.content, "html.parser", parse_only=strainer) # use the strainer to restrict parsing
+        url = soup.find("form")["action"] # grab the login url
+        creds = {"username": user, "password": passwd, "remember": 1} # set credentials (remember is irrelevant, due to this being a singular session)
+        resp2 = SESSION.post(url, data=creds) # hopefully logged in correctly
     except Exception as excp:
         LOGGER.exception(excp)
         LOGGER.warning(f"Something went wrong during Login!\nExiting...")
@@ -86,85 +88,86 @@ def init_preps():
         input("Please enter the URL of the Anime you want to download: "))
     #inputurl = "https://proxer.me/info/6587"#cm
     firstepisode = int(
-        float(
-            input("Please enter the Number of the first Episode you want: ") or 1))
+        input("Please enter the Number of the first Episode you want: ") or 1)
     lastepisode = int(
-        float(
-            input("Please enter the Number of the last Episode you want: ") or 1))
+        input("Please enter the Number of the last Episode you want: ") or 1)
 
-    if lastepisode <= firstepisode:
+    if lastepisode <= firstepisode: # check for fishy episode requests
         lastepisode = firstepisode
-    resp = SESSION.get(inputurl)
-    strainer = SoupStrainer(class_="fn")
+    resp = SESSION.get(inputurl) # grab the anime page
+    strainer = SoupStrainer(class_="fn") # let's restrict the area for our name search, to the exact element
     soup = BeautifulSoup(resp.content, "html.parser", parse_only=strainer)
-    name = soup.string
+    name = soup.string.replace(":", "-") # win compat qwq
 
     animedir = f"{CWD}{name}{SLASH}"
-    if not os.path.exists(animedir):
+    if not os.path.exists(animedir): # create anime directory
         os.mkdir(animedir)
 
     os.chdir(animedir)
-    match = search("#.*", inputurl)
+    match = search("#.*", inputurl) # check if the url contains unwanted resource descriptors
     if match is None:
         match = ""
     else:
-        match = match[0]
-    inputurl = inputurl.strip(match).replace("info", "watch")
-    if inputurl[-1:] != "/":
+        match = match[0] # there's a reason behind the urls scheme recommendation, if there's more than 1 match user should learn to read
+    inputurl = inputurl.strip(match).replace("info", "watch") # make sure it's the correct url (lazy)
+    if inputurl[-1:] != "/": # verify that "/" is the last char
         inputurl = f"{inputurl}/"
 
-    ftrlst = []
-
+    futurelist = []
     for episodenum in range(firstepisode, lastepisode + 1):
-        episodeurl = f"{inputurl}{episodenum}/engsub"
+        episodeurl = f"{inputurl}{episodenum}/engsub" # force the scrubs to enjoy engsub
         LOGGER.debug(episodeurl)
-        LOGGER.info(f"Creating Worker for Episode {episodenum}")
-        
-        ftrlst.append(EXECUTOR.submit(retrieve_source, episodeurl, name, episodenum))
+        LOGGER.debug(f"Creating Worker for Episode {episodenum}")
+        futurelist.append(EXECUTOR.submit(retrieve_source, episodeurl, name, episodenum))
 
-    for future in cf.as_completed(ftrlst):
+    for future in cf.as_completed(futurelist): # check for thread status
         try:
-            video = future.done()
-            LOGGER.info(f"Worker for Episode {episodenum} returned: {video}")
+            video = future.done() # cf equivalent of threading.Thread.join()
+            LOGGER.debug(f"Worker for Episode {episodenum} returned: {video}")
         except Exception as excp:
             LOGGER.exception(
                 f"{supposed_video} has thrown Exception:\n{excp}")
 
 
 def retrieve_source(episodeurl, name, episodenum):
-    """Function to make all the Magic happen"""
-    #LOGGER.info(f"{episodeurl}, {name}, {episodenum}")
-    streamhosterurl = None
-    resp = SESSION.get(episodeurl, timeout=30)
+    """ Function to make all the Magic happen, parses the streamhoster url [Proxer] and parses the video url """
+    try: # if anything fails in here, it's prolly the captcha
+        #LOGGER.info(f"{episodeurl}, {name}, {episodenum}")
+        streamhosterurl = None
+        resp = SESSION.get(episodeurl, timeout=30) # grab the specific episode
 
-    for line in resp.text.split("\n"):
-        if "var streams" in line:
-            #LOGGER.info(line.split("[{")[1].split("}];")[0].split("},{"))
-            for streamhoster in line.split("[{")[1].split("}];")[0].split("},{"):
-                elem = streamhoster.split("code\":\"")[1].split("\",\"img\"")[0].replace("//", "").replace(r"\/", "/").replace("\":\"", "\",\"").split("\",\"")
-                code = str(elem[0])
-                baseurl = f"{elem[8]}".replace("#", code)
-                if "http" not in baseurl:
-                    baseurl = f"http://{baseurl}" 
-                #LOGGER.info(f"Streamurls: {baseurl}")
-                if "proxer" in baseurl:
-                    streamhosterurl = baseurl
-    #LOGGER.info(f"Streamhoster: {streamhosterurl}")
+        for line in resp.text.split("\n"):
+            if "var streams" in line:
+                #LOGGER.info(line.split("[{")[1].split("}];")[0].split("},{"))
+                for streamhoster in line.split("[{")[1].split("}];")[0].split("},{"): # parses all available stream hoster
+                    elem = streamhoster.split("code\":\"")[1].split("\",\"img\"")[0].replace("//", "").replace(r"\/", "/").replace("\":\"", "\",\"").split("\",\"")
+                    code = str(elem[0])
+                    baseurl = f"{elem[8]}".replace("#", code)
+                    if "http" not in baseurl:
+                        baseurl = f"http://{baseurl}" 
+                    #LOGGER.info(f"Streamurls: {baseurl}")
+                    if "proxer" in baseurl: # we'll just use proxer tho
+                        streamhosterurl = baseurl
+        LOGGER.info(f"Streamhoster: {streamhosterurl}")
 
-    resp2 = SESSION.get(streamhosterurl, timeout=30)
-    for line in resp2.text.split("\n"):
-        if "\"http" and ".mp4\"" in line:
-            streamurl = f"http{line.split('http')[1].split('.mp4')[0]}.mp4"
-            episodename = f"{os.getcwd()}{SLASH}{name}_Episode_{episodenum}.mp4"
-            #LOGGER.info(f"Streamurl: {streamurl}")
-            if streamurl == "": # error check for captcha
-                return False
-            if not download_file(episodename, streamurl):
-                return False # error check for file previously downloaded/failed ?
+        if streamhosterurl == None:
+            raise NoURLError
+
+        resp2 = SESSION.get(streamhosterurl, timeout=30) # grabbing the page where the video is embedded in
+        for line in resp2.text.split("\n"):
+            if "\"http" and ".mp4\"" in line: # parsing the video url from that half-crappy js
+                streamurl = f"http{line.split('http')[1].split('.mp4')[0]}.mp4"
+                episodename = f"{os.getcwd()}{SLASH}{name}_Episode_{episodenum}.mp4"
+                #LOGGER.info(f"Streamurl: {streamurl}")
+                if streamurl == "": # verify this check!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11
+                    raise NoURLError()
+                download_file(episodename, streamurl)
+    except Exception as excp:
+        LOGGER.exception(f"{excp}")
 
 def __main__():
-    """MAIN"""
+    """ MAIN """
     init_preps()
 
-if __name__ == "__main__":
+if __name__ == "__main__": # main guard
     __main__()
